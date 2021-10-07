f5ed258836c28d638c14b40a8cc08d6d651a5e01
Thomas Cashman <thomas.cashman@viridiansoftware.com>
2021-10-07 16:00:00 +0100

    Fix build output not being removable after execution

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ sdk_name=renpy-$1-sdk
 echo "Building the project at $2..."
 if ../renpy/renpy.sh ../renpy/launcher distribute $2; then
     built_dir=$(ls | grep '\-dists')
+    chmod -R 777 $built_dir
     echo ::set-output name=dir::$built_dir
     echo ::set-output name=version::${built_dir%'-dists'}
 else
